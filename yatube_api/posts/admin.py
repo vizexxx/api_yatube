09a827f3012a03c325a3cb7ba256418e7bc2d2a2
Valeria Melnikova <150736925+vizexxx@users.noreply.github.com>
2025-11-23 16:07:58 +0300

    fix

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
 
-from .models import Comment, Group, Post
+from .models import Group, Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = (
+        'pk',
+        'pub_date',
+        'author',
+        'group',
+        'image',
+    )
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
-admin.site.register(Comment)
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'description',
+    )
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('slug',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
